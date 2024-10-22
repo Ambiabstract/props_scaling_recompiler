c1f7542c6e33ef1808c726854195d3855dcc7046
Sergey Shavin <60753651+Ambiabstract@users.noreply.github.com>
2024-10-23 03:58:26 +0400

    Update props_scaling_recompiler_make_exe.bat

--- props_scaling_recompiler_make_exe.bat
+++ props_scaling_recompiler_make_exe.bat
@@ -1,8 +1,8 @@
 pyinstaller --onefile --name=props_scaling_recompiler --icon=props_scaling_recompiler_icon_v1.ico props_scaling_recompiler.py
 
-rem set source="dist\props_scaling_recompiler.exe"
-rem set destination="C:\Program Files (x86)\Steam\steamapps\common\Half-Life 2\bin"
+set source="dist\props_scaling_recompiler.exe"
+set destination="C:\Program Files (x86)\Steam\steamapps\common\Half-Life 2\bin"
 
-rem xcopy /y %source% %destination%
+xcopy /y %source% %destination%
 
 pause
