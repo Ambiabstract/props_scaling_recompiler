b30c2abc619ae95260790d7b2155caeb3d0aad0b
Sergey Shavin <60753651+Ambiabstract@users.noreply.github.com>
2025-11-24 00:39:49 +0400

    Create mdl_skins_and_cdmaterials.bat

---
+++ mdl_skins_and_cdmaterials.bat
@@ -0,0 +1,10 @@
+REM python mdl_skins_and_cdmaterials.py "c:\program files (x86)\steam\steamapps\sourcemods\antenna_mod\models\props_se\propper\metal_cup_mug.mdl"
+
+python mdl_skins_and_cdmaterials.py "c:\program files (x86)\steam\steamapps\sourcemods\antenna_mod\models\weapons\v_colt.mdl"
+
+REM python mdl_skins_and_cdmaterials.py "c:\program files (x86)\steam\steamapps\sourcemods\antenna_mod\models\props_se\storage\book_2.mdl"
+
+
+REM python mdl_skins_and_cdmaterials.py "c:\program files (x86)\steam\steamapps\sourcemods\antenna_mod\models\props_se\uaz452\uaz452.mdl"
+
+pause
