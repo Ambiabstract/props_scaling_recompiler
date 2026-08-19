b64da3183636134cba2567c3e76c7aaa15e79e43
Ambiabstract <60753651+Ambiabstract@users.noreply.github.com>
2026-08-19 23:53:35 +0300

    Update props_scaling_recompiler_make_exe_nopause.bat

--- props_scaling_recompiler_make_exe_nopause.bat
+++ props_scaling_recompiler_make_exe_nopause.bat
@@ -1,8 +1,6 @@
 pyinstaller --onefile --name=props_scaling_recompiler --icon=props_scaling_recompiler_icon_v1.ico props_scaling_recompiler.py
 
 set source="dist\props_scaling_recompiler.exe"
-set destination="C:\Program Files (x86)\Steam\steamapps\common\Half-Life 2\bin"
+set destination="C:\Program Files (x86)\Steam\steamapps\common\Source SDK Base 2013 Singleplayer\bin"
 
 xcopy /y %source% %destination%
-
-rem pause
